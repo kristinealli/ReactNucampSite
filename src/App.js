48cf901fb5827697645be25b16d6fbe482e25198
Kristine Johnson <kristineallisonjohnson@outlook.com>
2023-01-18 00:48:19 -0500

    AnimatedDisplayCard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Routes, Route} from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
+import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
 import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+
 
 function App() {
     return (
@@ -13,7 +16,12 @@ function App() {
             <Routes>
                 <Route path='/' element={<HomePage />} />
                 <Route path='contact' element={<ContactPage />} />
+                <Route path='about' element={<AboutPage />} />
                 <Route path='directory' element= {<CampsitesDirectoryPage />} />
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
             </Routes>
             <Footer/>
         </div>
